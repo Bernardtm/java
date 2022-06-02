788de9597354a3cfb614133afc69b803b597c157
Bernard Mendes <bernardtm.dev@gmail.com>
2022-06-01 21:53:01 -0300

    collections and types

---
+++ collections/Collections.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Collections {
+    public static void main(String[] args) {
+        //Set<String> set = new SortedSet<>();
+        List<String> list = new ArrayList<>();
+        String[] array = new String[7];
+        HashMap<Long, String> hashMap = new HashMap<>();
+        Map<Long, String> map = new TreeMap<>();
+        System.out.println("Collections!"); 
+    }
+}
---
+++ types/Types.java
@@ -0,0 +1,33 @@
+import java.math.BigDecimal;
+
+public class Types {
+    public static void main(String[] args) {
+        int primitiveInteger = 0;
+        System.out.println(primitiveInteger);
+        long primitiveLong = 0;
+        System.out.println(primitiveLong);
+        float primiteFloat = 0;
+        System.out.println(primiteFloat);
+        double primitiveDouble = 0;
+        System.out.println(primitiveDouble);
+        char character = 'c';
+        System.out.println(character);
+        boolean primitiveBoolean = false;
+        System.out.println(primitiveBoolean);
+
+        Integer intergerWrapper = 1;
+        System.out.println(intergerWrapper);
+        Long longWrapper = 0L;
+        System.out.println(longWrapper);
+        Float floatWrapper = 0f;
+        System.out.println(floatWrapper);
+        Double doubleWrapper = 0.0;
+        System.out.println(doubleWrapper);
+        Boolean booleanWrapper = false;
+        System.out.println(booleanWrapper);
+        BigDecimal bigDecimal = new BigDecimal(0.0);
+        System.out.println(bigDecimal);
+        String string = new String();
+        System.out.println(string);
+    }
+}
